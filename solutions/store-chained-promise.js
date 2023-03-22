203380b7782231f2f45224c56da87bd8345f86d9
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2023-03-22 13:21:02 -0400

    Test another possible solution.

---
+++ solutions/store-chained-promise.js
@@ -0,0 +1,19 @@
+document.getElementById("make-class-leak").onclick = () => {
+  const myClassInstance = new MyClass();
+  myClassInstance.myAsyncMethod(); // <-- each MyClass instance on which you call myAsyncMethod will leak.
+};
+
+class MyClass {
+  async myAsyncMethod() {
+    const result = await getAndCachePromise();
+    console.log("myAsyncMethod ran with promise result: ", result);
+  }
+}
+
+const promiseCache = [];
+
+function getAndCachePromise() {
+  const myPromise = new Promise((resolve) => resolve("<PromiseResult>"));
+  promiseCache.push(myPromise.then());
+  return myPromise;
+}
